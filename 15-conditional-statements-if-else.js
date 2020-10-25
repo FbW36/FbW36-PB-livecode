df36096471242d9fa80109d49ebce3d1f9b76813
Ghassan Aldarwish <ghassanaldarwish@Ghassans-MacBook-Pro.local>
2020-10-26 00:38:57 +0100

    Conditional Statements if-else

---
+++ 15-conditional-statements-if-else.js
@@ -0,0 +1,120 @@
+// Conditional Statements if-else
+
+// Logical Thinking:
+//Sunny
+// 1. if sunny dress in shorts and light top
+// 2. if sunny wear a face cap or hat
+//Rainy
+// 1. if rainy take along an umbrella
+// 2. if rainy wear a rainboot
+// 3. if rainy use a car where possible
+// Cloudy
+// 1. if cloudy  dress in your finnest as possible
+// 2. if cloudy  wear any sheos of your choice
+// Snowy
+// 1. if snow wear a winter jacket or mantle
+// 2. if snow wear a snow boot
+// 3. if snow check the traffic before going out
+
+/********************************************************/
+
+// Sometimes, we need to perform different actions based on different conditions.
+// The if(...) statement evaluates a condition in parentheses and, if the result is true, executes a block of code.
+// The if (…) statement evaluates the expression in its parentheses and converts the result to a boolean.
+
+/********************************************************/
+
+// 1.
+// Input: 21 and 3.
+// if 21 is divisible by 3 then print- Yes, 21 is a multiple of 3
+// if 21 is not divisible by 3 then print- No, 21 is not a multiple of 3
+
+/********************************************************/
+
+// 2.
+// Input: varA and varB
+// if varA is not equal to varB, print- Booleans are not equal to strings!
+// else, print- Booleans are equal to strings!
+
+/********************************************************/
+// 3.
+// We can also pass a pre-evaluated boolean value to if, like this:
+
+// Input: year and cond
+// If the condition is true, then print - Yes, it is 2020.
+// else, print- You are not up to date.
+
+/********************************************************/
+// 4.
+// Sometimes, we need to assign a variable depending on a condition.
+
+// Input: age
+// if age is grater than 18, then allow access (accessAllowed is true)
+// else deny access (accessAllowed is false)
+
+/********************************************************/
+
+// 5.
+
+// Input: company, language, statementTrue and statementFalse
+// if the company is Netscape, print true statement
+// else print false statement
+
+/********************************************************/
+// 7.
+
+// Inputs:
+// weather
+// tan
+
+// Possible Outputs:
+// "Don't forget your sunglasses!"
+// "Don't forget your sunblocker"
+// "enjoy!"
+// "Don't forget your umbrella!"
+
+// if weather is sunny OR weather is snowy, print - "Don't forget your sunglasses!"
+// if you don't want a tan, print "Don't forget your sunblocker", else print "enjoy!"
+// else print "Don't forget your umbrella!"
+
+/*####################################################*/
+// Several conditions: “else if”
+
+// 1.
+
+// Input:
+// time2
+
+// If time is less than 10, greeting is "Good morning!", print greeting.
+// If time is grater than/equal to 10 AND less than 20, greeting is "Good day!", print greeting.
+// Else greeting is "Good evening!", print greeting.
+
+/********************************************************/
+// 2.
+let firstName = "John";
+let agE = 25;
+
+// Input: first name and age
+
+// If age is less than 13, print ${firstname} is a kid.
+// If age is grater than/equal to 13 AND less than 20, print ${firstname} is a teenager.
+// If age is grater than/equal to 20 AND less than 30, print ${firstname} is a young man.
+// Else print ${firstname} is a man.
+
+/********************************************************/
+// 3.
+
+// Inputs:
+// weather
+// tan
+
+// Possible Outputs:
+// "Don't forget your sunglasses!"
+// "Don't forget your sunblocker"
+// "enjoy!"
+// "Don't forget your umbrella!"
+
+// if weather is sunny, print - "Don't forget your sunglasses!"
+// if weather is sunny and you don't want a tan, print "Don't forget your sunblocker", else print "enjoy!"
+// if weather is snowy, print - "Don't forget your coat!"
+// else print "Don't forget your umbrella!"
