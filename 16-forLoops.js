37448248f1daac81432e7394a3b9d71ce07b5cc4
Ghassan Aldarwish <ghassanaldarwish@Ghassans-MacBook-Pro.local>
2020-10-27 08:43:27 +0100

    for loop

---
+++ 16-forLoops.js
@@ -0,0 +1,31 @@
+// For loop statement
+
+// Loops are used to repeat our actions.
+
+// In computer sicience, a loop is a sequence of instructions that is continually repeated until a certain condition is met.
+
+// ["img1", "img2", "img3", "img4", "img5", "img6", "img7", "img8"]
+
+//  for loop
+
+//  syntax: for(begin; condition; step) { ....render the image.... }
+
+// begin(i = 0) : Executes once upon entering the loop.
+
+// condition(i <= 3): Checked before every loop iteration. If false, the loop stops.
+
+// body({console.log(i)}): Runs again and again while the condition is truthy.
+
+// step(i++): Executes after the body on each iteration.
+
+/*********************************************/
+// Tasks:
+/*********************************************/
+
+// 1. Print the value of i if i starts from 0 and i is less than 3.
+
+/*********************************************/
+
+// 2. Print the value of i if i starts from 1 and i is less than/equal to 3.
+
+/*********************************************/
