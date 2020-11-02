02c961d2f737eba08b5565d1589bf6c86502bbed
Ghassan Aldarwish <ghassanaldarwish@Ghassans-MacBook-Pro.local>
2020-11-02 08:40:35 +0100

    arrow functions template

---
+++ 17-functions/4-arrowFunctions.js
@@ -0,0 +1,9 @@
+// Arrow Functions are very convenient for simple one-line actions, when we’re just too lazy to write many words.
+
+// The examples below take arguments from the left of => and evaluated the right-side expression with them.
+
+/*
+Arrow functions are handy for one-liners. They come in two flavors:
+Without curly braces: (...args) => expression – the right side is an expression: the function evaluates it and returns the result.
+With curly braces: (...args) => { body } – brackets allow us to write multiple statements inside the function, but we need an explicit return to return something.
+*/
