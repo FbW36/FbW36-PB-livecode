93750b2bb33e80196cd701aea89ff07dea289155
Ghassan Aldarwish <ghassanaldarwish@Ghassans-MacBook-Pro.local>
2020-11-05 10:22:41 +0100

    school app template updated

--- schoolApp.js
+++ schoolApp.js
@@ -53,7 +53,7 @@ school data model:
    the class model: 
  {
     name: "FbW3",
-    students: [studentsObject1, studentsObject1,...],
+    students: [studentObject1, studentObject1,...],
   }
  */
 
@@ -63,27 +63,112 @@ school data model:
         name: "Pilar",
         email: "pilar@yahoo.com",
         city: "Berlin",
-      }
+  }
  */
+//------------##############################-----------
+// school store
+const school = [
+  {
+    name: "Fbw1",
+    students: [
+      {
+        name: "Alex",
+        email: "alex@yahoo.com",
+        city: "Berlin",
+      },
+      {
+        name: "Max",
+        email: "max@yahoo.com",
+        city: "Hamburg",
+      },
+    ],
+  },
+  {
+    name: "FbW2",
+    students: [
+      {
+        name: "Jon",
+        email: "jon@yahoo.com",
+        city: "Berlin",
+      },
+      {
+        name: "Pilar",
+        email: "pilar@yahoo.com",
+        city: "Berlin",
+      },
+    ],
+  },
+  {
+    name: "FbW3",
+    students: [],
+  },
+];
 //------------##############################-----------
 // App Functions
 //! create class
 //! create student
 
 //! read
+// create function call RenderSchoolTemplate
+// This function Format and render school data
+
+const RenderSchoolTemplate = () => {
+  let template = "";
+  template += "School Classes" + "\n";
+  template += "--------------" + "\n";
+  let totalStudents = 0;
+  if (school.length < 1) {
+    // if the school array is empty
+    template += "  The school is empty!" + "\n";
+  } else {
+    // if we have class/es in the school
+    for (let i = 0; i < school.length; i++) {
+      let classData = school[i];
+      template += ` ${classData.name} - (class ID: ${i + 1}):  \n`;
+      // console.log("classData.students ==> ", classData.students);
+      if (classData.students.length < 1) {
+        template += "   The class is empty!" + "\n";
+      } else {
+        for (let j = 0; j < classData.students.length; j++) {
+          let student = classData.students[j];
+          /**
+           * EX:  Alex, alex@yahoo.com, Berlin - (student ID: 1).
+            student ==>  { name: 'Alex', email: 'alex@yahoo.com', city: 'Berlin' }
+            student ==>  { name: 'Max', email: 'max@yahoo.com', city: 'Hamburg' }
+            student ==>  { name: 'Jon', email: 'jon@yahoo.com', city: 'Berlin' }
+            student ==>  { name: 'Pilar', email: 'pilar@yahoo.com', city: 'Berlin' }
+       */
+          template += `  ${j + 1}- ${student.name}, ${student.email}, ${
+            student.city
+          } (student ID: ${j + 1}) \n`;
+
+          totalStudents += 1;
+        }
+      }
+      template += "***************************" + "\n";
+    }
+  }
+
+  template +=
+    "   Total Classes " + school.length + ", total students " + totalStudents;
+
+  return template;
+};
 
 //------------#########################-----------
 // Call Stack
 
 //--------------------
 // render school template
-
+console.log(RenderSchoolTemplate());
 //--------------------
 
 //------------###########-----------
 
 /**
  
+Final Template 
+
  School Classes: 
 ------------------ 
  FbW1 - (class ID: 1): 
