01cf667552f2f1bd0b78653160b3ca5d8be3d33f
Ghassan Aldarwish <ghassanaldarwish@Ghassans-MacBook-Pro.local>
2020-11-05 08:48:09 +0100

    school app template

---
+++ schoolApp.js
@@ -0,0 +1,102 @@
+// School app
+// create app to add, remove, read and edit Students and Classes in a School
+
+/**
+ 
+school data model:
+
+ [
+  {
+    name: "FbW1",
+    students: [
+      {
+        name: "Alex",
+        email: "alex@yahoo.com",
+        city: "Berlin",
+      },
+      {
+        name: "Max",
+        email: "max@yahoo.com",
+        city: "Hamburg",
+      },
+    ],
+  },
+
+  {
+    name: "FbW2",
+    students: [
+      {
+        name: "Jon",
+        email: "jon@yahoo.com",
+        city: "Berlin",
+      },
+      {
+        name: "Pilar",
+        email: "pilar@yahoo.com",
+        city: "Berlin",
+      },
+    ],
+  },
+  {
+    name: "FbW3",
+    students: [],
+  },
+]
+ */
+
+/**
+   the school model: 
+   [ classObject1,classObject2,....]
+ */
+
+/**
+   the class model: 
+ {
+    name: "FbW3",
+    students: [studentsObject1, studentsObject1,...],
+  }
+ */
+
+/**
+   the Student model: 
+  {
+        name: "Pilar",
+        email: "pilar@yahoo.com",
+        city: "Berlin",
+      }
+ */
+//------------##############################-----------
+// App Functions
+//! create class
+//! create student
+
+//! read
+
+//------------#########################-----------
+// Call Stack
+
+//--------------------
+// render school template
+
+//--------------------
+
+//------------###########-----------
+
+/**
+ 
+ School Classes: 
+------------------ 
+ FbW1 - (class ID: 1): 
+  1- Alex, alex@yahoo.com, Berlin - (student ID: 1).
+  2- Max, max@yahoo.com, Hamburg - (student ID: 2).
+******************************************** 
+ FbW2 - (class ID: 2): 
+  1- Jon, jon@yahoo.com, Berlin - (student ID: 1).
+  2- Pilar, pilar@yahoo.com, Berlin - (student ID: 2).
+******************************************** 
+ FbW3 - (class ID: 3): 
+   The class is empty 
+******************************************** 
+  Total Classes 3, total students 4
+
+ */
