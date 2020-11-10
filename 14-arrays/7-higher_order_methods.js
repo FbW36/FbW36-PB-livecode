787bb92cd5609f118870fae1e3b50736ddc72453
Ghassan Aldarwish <ghassanaldarwish@Ghassans-MacBook-Pro.local>
2020-11-10 12:14:51 +0100

    filter, reduce and sort

--- 14-arrays/7-higher_order_methods.js
+++ 14-arrays/7-higher_order_methods.js
@@ -81,3 +81,200 @@ console.log("oddNumbersRes ==> ", oddNumbersRes);
   7, 27,  7, 3, 7
 ]
  */
+
+//---------##############---------
+//2. The filter() method creates a new array with all elements that pass the test implemented by the provided function.
+
+const numbers3 = [2, 3, 4, 5, 6, 7, 1];
+
+const filteredNumbers3 = numbers3.filter((item) => item > 4);
+
+console.log("filteredNumbers3 ==> ", filteredNumbers3); // [ 5, 6, 7 ]
+
+//----------#######--------
+
+const numbers4 = [
+  [1, 2, 1, 24],
+  [8, 11, 9, 4],
+  [7, 0, 7, 27],
+  [7, 4, 28, 14],
+  [3, 10, 26, 7],
+  [88, 4],
+];
+
+const oddNumbersRes2 = numbers4.map((item) =>
+  item.filter((num) => num % 2 === 1)
+);
+
+console.log("oddNumbersRes2 => ", oddNumbersRes2); //   [ [ 1, 1 ], [ 11, 9 ], [ 7, 7, 27 ], [ 7 ], [ 3, 7 ], [] ]
+
+//--------------##########----------------
+
+//3. The reduce() method executes a reducer function (that you provide) on each member of the array resulting in a single output value.
+
+// Get the sum of the numbers in numbersToReduce array
+const numbersToReduce = [1, 2, 3, 4, 5, 6];
+
+// first way to solve the task
+let box = 0;
+for (let item of numbersToReduce) {
+  box += item;
+}
+
+console.log("box ===> ", box); // 21
+
+// 2nd to solve the task
+
+let box2 = 0;
+
+numbersToReduce.map(function (item) {
+  box2 += item;
+});
+
+console.log("box2 ===> ", box2); // 21
+
+/// 3rd way to solve the task with reduce
+
+const sum = numbersToReduce.reduce((box, item) => box + item, 0);
+console.log("sum ==> ", sum); // 21
+
+// new task ==> add Hallo Before each item in names array ==> out array
+const names2 = ["Alex", "Jon", "Tommy"];
+
+const newNames2 = names2.reduce((box, item) => {
+  box.push("Hallo " + item);
+  return box;
+}, []);
+console.log("newNames2 ==> ", newNames2); // [ 'Hallo Alex', 'Hallo Jon', 'Hallo Tommy' ]
+
+//#### is better to use map
+
+const newNames3 = names2.map((item) => "Hallo " + item);
+console.log("newNames3 ==> ", newNames3); // [ 'Hallo Alex', 'Hallo Jon', 'Hallo Tommy' ]
+
+//-------------##########----------------
+
+// Get the odd numbers and put them in one array
+const nestedNumbers = [
+  [1, 2, 1, 24],
+  [8, 11, 9, 4],
+  [7, 0, 7, 27],
+  [7, 4, 28, 14],
+  [3, 10, 26, 7],
+  [88, 4],
+];
+
+const oddNumbersWithReduce = nestedNumbers.reduce(function (box, item) {
+  console.log("item ==> ", item);
+  /**
+    item ==>  [ 1, 2, 1, 24 ]
+    item ==>  [ 8, 11, 9, 4 ]
+    item ==>  [ 7, 0, 7, 27 ]
+    item ==>  [ 7, 4, 28, 14 ]
+    item ==>  [ 3, 10, 26, 7 ]
+    item ==>  [ 88, 4 ]
+   */
+  item.map((num) => {
+    console.log("num ==> ", num);
+    if (num % 2 === 1) {
+      box.push(num);
+    }
+  });
+  return box;
+}, []);
+
+console.log("oddNumbersWithReduce ==> ", oddNumbersWithReduce);
+/**
+ [
+  1,  1, 11, 9, 7,
+  7, 27,  7, 3, 7
+]
+ */
+
+/**
+ 
+  INPUT
+[
+  [1, 2, 1, 24],
+  [8, 11, 9, 4],
+  [7, 0, 7, 27],     
+  [7, 4, 28, 14],   
+  [3, 10, 26, 7],
+  [88, 4],
+];
+
+   OUTPUT
+     
+   {
+     odd1: 1,
+     odd2: 1,
+     odd3: 11,
+     odd4: 9,
+     odd5: 7,
+     odd6: 7,
+     odd7 : 27,
+     odd8: 7,
+     odd9: 3,
+     odd10:7
+   }
+
+ */
+
+const INPUT_ARRAY = [
+  [1, 2, 1, 24],
+  [8, 11, 9, 4],
+  [7, 0, 7, 27],
+  [7, 4, 28, 14],
+  [3, 10, 26, 7],
+  [88, 4],
+];
+
+//Start from 1 and Increment by 1 with each Iteration
+let order = 1;
+
+// To reduce INPUT_ARRAY to object
+
+function reducerFunction(box, item) {
+  //console.log("reducerFunction ==> item ==> ", item);
+
+  item.map((numInItem) => {
+    //console.log("reducerFunction ==> item ==> map ==> numInItem ==> ", numInItem);
+
+    if (numInItem % 2 === 1) {
+      //console.log("reducerFunction ==> item ==> map ==> numInItem ==> if(numInItem % 2 === 1) ==> ", numInItem)
+      box["odd" + order++] = numInItem;
+    }
+  });
+  return box;
+}
+const oddNumbersObject = INPUT_ARRAY.reduce(reducerFunction, {});
+
+console.log("oddNumbersObject ==> ", oddNumbersObject);
+
+//---------------######------------
+
+//4. The sort() method sorts the elements of an array in place and returns the sorted array.
+
+const arrayToSort = [3, 1, 66];
+
+const sortedArray = arrayToSort.sort((test1, test2) => {
+  console.log("sortedArray ==>  test1 ==> ", test1);
+  console.log("sortedArray ==>  test2 ==> ", test2);
+
+  /**
+    sortedArray ==>  test1 ==>  1
+    sortedArray ==>  test2 ==>  3
+    sortedArray ==>  test1 ==>  66
+    sortedArray ==>  test2 ==>  1
+    sortedArray ==>  test1 ==>  66
+    sortedArray ==>  test2 ==>  1
+    sortedArray ==>  test1 ==>  66
+    sortedArray ==>  test2 ==>  3
+   */
+  // return test1 - test2; 0 - 99
+  return test2 - test1; // 99 - 0
+});
+
+console.log("sortedArray ==> ", sortedArray);
+
+//
