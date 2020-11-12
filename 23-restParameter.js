e6ffe8fb7de63bd600e99531969342b1f41e0768
Ghassan Aldarwish <ghassanaldarwish@Ghassans-MacBook-Pro.local>
2020-11-12 12:02:25 +0100

    Rest parameter

--- 23-restParameter.js
+++ 23-restParameter.js
@@ -45,16 +45,16 @@ const sandwich2 = {
   cheese: "swiss",
   toppings: ["lettuce", "tomato", "mustard"],
 };
-//const bread = sandwich.bread; ==> ref
-//const meat = sandwich.meat; ==> ref
-let { bread, meat } = sandwich2; // clone
-bread = "garlic";
-meat = "turkey";
-console.log("{ bread, meat } clone==> ", `bread: ${bread}, meat: ${meat}`);
+//let bread = sandwich2.bread;
+//let meat = sandwich2.meat;
+
+let { bread, meat, toppings, test } = sandwich2;
+test = "Alex";
 console.log(
-  " sandwich2 origin==> ",
-  `bread: ${sandwich2.bread}, meat: ${sandwich2.meat}`
+  `bread: ${bread}, meat: ${meat}, toppings: ${toppings}, test: ${test}`
 );
+// bread: dutch crunch, meat: tuna, toppings: lettuce,tomato,mustard, test: Alex
+
 //-------------------#########-----------
 
 /**
@@ -62,13 +62,27 @@ console.log(
    Values can also be destructured from arrays. Imagine that we wanted to assign the first value of an array to a variable name:
  */
 
-const [firstAnimal] = ["Horse", "Mouse", "Cat"];
+//const [firstAnimal] = ["Horse", "Mouse", "Cat"];
+const animals = ["Horse", "Mouse", "Cat"];
+
+//let animals_1 = animals[0];
+//let animals_2 = animals[1];
+//let animals_3 = animals[2];
+
+let [animals_1, animals_2, animals_3] = animals;
+
+console.log(
+  `animals_1 ==> ${animals_1}, animals_2 ==> ${animals_2}, animals_3 ==> ${animals_3}`
+);
+// animals_1 ==> Horse, animals_2 ==> Mouse, animals_3 ==> Cat
 
 /**
  We can also pass over unnecessary values with list matching using commas. List matching occurs when commas take the place of elements that should be skipped. With the same array, we can access the last value by replacing the first two values with commas:
  */
 
-const [, , thirdAnimal] = ["Horse", "Mouse", "Cat"];
+const [, , item3] = ["Horse", "Mouse", "Cat"];
+
+console.log("item3 ==> ", item3);
 
 //------------#######---------------
 
@@ -76,7 +90,9 @@ const [, , thirdAnimal] = ["Horse", "Mouse", "Cat"];
 // The spread operator can also be used to get the remaining items in the array:
 
 const lakes = ["Donner", "Marlette", "Fallen Leaf", "Cascade"];
-const [first, ...others] = lakes;
+const [item1, ...restItems] = lakes;
+
+console.log("restItems ==> ", restItems); // [ 'Marlette', 'Fallen Leaf', 'Cascade' ]
 
 //-----------##########--------------
 
@@ -85,9 +101,13 @@ const morning = {
   lunch: "peanut butter and jelly",
 };
 const dinner = "mac and cheese";
-const backpackingMeals = {
-  ...morning,
-  dinner,
-};
+const backpackingMeals = { ...morning, dinner };
+/**
+ {
+  breakfast: 'oatmeal',
+  lunch: 'peanut butter and jelly',
+  dinner: 'mac and cheese'
+}
+ */
 
-// console.log(backpackingMeals);
+console.log(backpackingMeals);
