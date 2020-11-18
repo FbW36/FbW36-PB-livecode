cd20296522c389620c9a1105f6cb985edeee94b9
Ghassan Aldarwish <ghassanaldarwish@Ghassans-MacBook-Pro.local>
2020-11-18 09:02:42 +0100

    clone vs ref

---
+++ 25-cloneVSreference.js
@@ -0,0 +1,24 @@
+/**
+ Primitives
+- If a primitive type is assigned to a variable, we can think of that variable as containing the primitive value.
+
+- When we assign these variables to other variables using =, we copy the value to the new variable. They are copied by value.
+
+- Changing one does not change the other. Think of the variables as having no relationship to each other.
+ */
+
+//---------------#################------------
+
+/**
+  Reference data type Objects And Arrays
+
+  -Variables that are assigned a non-primitive value are given a reference to that 
+  value. That reference points to the object’s location in memory. The variables don’t actually contain the value.
+  */
+
+//----------------#######--------------------------
+
+/**
+   Clone 
+   - We can clone with Spread Operator  It’s work with objects and Arrays 
+   */
